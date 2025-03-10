ab2c029fc46efc94b74b5d226e56f0a7e853d0c8
Jhorman Salazar <salazarjhorman181@gmail.com>
2025-03-09 21:36:40 -0500

    Add files via upload

---
+++ platzi-market-jh/src/main/java/com/platzi/market_jh/PlatziMarketJhApplication.java
@@ -0,0 +1,13 @@
+package com.platzi.market_jh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziMarketJhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziMarketJhApplication.class, args);
+	}
+
+}
---
+++ platzi-market-jh/src/test/java/com/platzi/market_jh/PlatziMarketJhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platzi.market_jh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatziMarketJhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
